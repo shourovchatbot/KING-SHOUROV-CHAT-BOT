70edf9a261f47975525c7468d63de3d50a1b059c
shourovchatbot <shourovchatbot@gmail.com>
2024-12-02 20:57:18 +0300

    Update Fahim1234.js

--- scripts/commands/Fahim1234.js
+++ scripts/commands/Fahim1234.js
@@ -26,7 +26,7 @@ const media = (
 
 	if (body.indexOf("💦")==0 || body.indexOf("valo")==0 || body.indexOf("ভালোবাসা")==0 || body.indexOf("মায়া")==0 || body.indexOf("Maya")==0 || body.indexOf("Maya")==0 || body.indexOf("maya")==0 || body.indexOf("maya")==0 || body.indexOf("maya")==0 || body.indexOf(",")==0) {
 		var msg = {
-				body: "Md Fahim islam",
+				body: "KING SHOUROV",
 				attachment: media
 			}
 			api.sendMessage( msg, threadID, messageID);
