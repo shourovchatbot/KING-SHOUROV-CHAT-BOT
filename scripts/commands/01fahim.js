786821d5765aa25f320b5839599cf6797e5e47ba
shourovchatbot <shourovchatbot@gmail.com>
2024-12-02 20:08:53 +0300

    Update 01fahim.js

--- scripts/commands/01fahim.js
+++ scripts/commands/01fahim.js
@@ -19,7 +19,7 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const axios = require('axios')
 const media = (
     await axios.get(
-      'https://imgur.com/a/DkHQBji',
+      'https://imgur.com/tUuTeXT',
       { responseType: 'stream' }
     )
   ).data;
