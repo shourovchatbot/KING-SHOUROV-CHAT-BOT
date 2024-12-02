5b85463f779a49d0963c285d0f35a1446d206f78
shourovchatbot <shourovchatbot@gmail.com>
2024-12-02 21:08:46 +0300

    Update Fahim6.js

--- scripts/commands/Fahim6.js
+++ scripts/commands/Fahim6.js
@@ -26,7 +26,7 @@ const media = (
 
 	if (body.indexOf("bura beti")==0 || body.indexOf("😵‍💫")==0) {
 		var msg = {
-				body: "Md Fahim islam ",
+				body: "KING SHOUROV ",
 				attachment: media
 			}
 			api.sendMessage( msg, threadID, messageID);
