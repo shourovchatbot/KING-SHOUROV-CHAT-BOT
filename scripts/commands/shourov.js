ec6b1be432630a98e588536dc4d4b127e9d1b7f8
shourovchatbot <shourovchatbot@gmail.com>
2024-12-02 19:28:24 +0300

    Update shourov.js

--- scripts/commands/shourov.js
+++ scripts/commands/shourov.js
@@ -19,7 +19,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	if (body.indexOf("king")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("😒")==0) {
 		var msg = {
 				body: "এঁভাঁবেঁ তাঁকাঁসঁ নাঁ প্রেঁমেঁ পঁরেঁ যাঁবোঁ 😚🥀",
-				attachment: fs.createReadStream(__dirname + `/Nayan/এতো ভালোবাসা কই পাও .m4a.mp3`)
+				attachment: fs.createReadStream(__dirname + `/Nayan/এতো ভালোবাসা কই পাও .m4a`)
 			}
 			api.sendMessage( msg, threadID, messageID);
     api.setMessageReaction("😁", event.messageID, (err) => {}, true)
